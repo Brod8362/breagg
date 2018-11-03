ecfbdbbc43d62eba59f31ea5957a35acdded0f65
Brod8362 <brod8362@gmail.com>
2018-11-03 13:10:39 -0500

    Added CameraDisplay class.

--- android/app/src/main/java/pw/byakuren/breagg/MainActivity.java
+++ android/app/src/main/java/pw/byakuren/breagg/MainActivity.java
@@ -1,10 +1,16 @@
 package pw.byakuren.breagg;
 
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Camera cam;
+    FrameLayout layout;
+    CameraDisplay display;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -16,10 +22,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        // Define frame layout.
+        layout = (FrameLayout)findViewById(R.id.FrameLayout);
+
+        /*
+         * Yes, the Camera from android.hardware is deprecated, however
+         * it should be good enough for these needs.
+         */
+        cam = Camera.open();
+        display = new CameraDisplay(this, cam);
+
+
     }
+
+
+
+
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
