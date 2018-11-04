6d88397030093f83bd72219dd7c43b0e66830ec6
Brod8362 <brod8362@gmail.com>
2018-11-04 06:16:56 -0600

    removed deprecated code

--- android/app/src/main/java/pw/byakuren/breagg/MainActivity.java
+++ android/app/src/main/java/pw/byakuren/breagg/MainActivity.java
@@ -175,15 +175,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         ArrayList<NNOutput> array = imgclass.classifyFrame(bitmp);
-        /*
-        for (NNOutput obj: array) {
-            if (obj.getName() == "bread") {
-                setBreadMulti((int) Math.round(obj.getValue()*100));
-            } else {
-                setEggMulti((int) Math.round(obj.getValue()*100));
-            }
-        }
-        */
+        
         NNOutput egg = array.get(0);
         NNOutput bread = array.get(1);
         setBreadMulti((int) Math.round(bread.getValue()*100));
